96648916d1e489ef96c62d4b34ae0358e29d06b6
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2019-12-29 08:35:25 -0500

    test_all.sh: Fix typo

--- test_all.sh
+++ test_all.sh
@@ -8,7 +8,7 @@ case $? in
     0)
         # Has license
         sage -c 'load("patch_into_sage_module.py")' || exit 1
-        sage -c 'load("sage check_get_solver_with_name.py")' || exit 1
+        sage -c 'load("check_get_solver_with_name.py")' || exit 1
         sage -c 'load("check_sage_testsuite.py")' || echo "Ignoring errors from the sage test suite"
         exit 0
         ;;
