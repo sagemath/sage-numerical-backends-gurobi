5283a5f3c2d27b2453f4ba19b94d5f3f96941736
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2019-12-30 17:53:53 -0500

    check_sage_testsuite: Use sage.doctest.control module instead of calling out to sage-runtest. No longer necessary to patch before testing

--- check_sage_testsuite.py
+++ check_sage_testsuite.py
@@ -1,15 +1,46 @@
-# Check portions of the sage test suite -- call this after patch_into_sage_module.py
-import os
-import sage.env
+"""
+Check portions of the sage test suite, with default mip solver set to ours.
+
+    sage: M = MixedIntegerLinearProgram()
+    sage: M.get_backend()
+    <...sage_numerical_backends_gurobi...>
+"""
+
+import sage_numerical_backends_gurobi.gurobi_backend as gurobi_backend, sage.numerical.backends as backends, sys
+sys.modules['sage.numerical.backends.gurobi_backend'] = backends.gurobi_backend = gurobi_backend
+
+from sage.numerical.backends.generic_backend import default_mip_solver
+default_mip_solver('Gurobi')
+
+from sage.doctest.control import DocTestController, DocTestDefaults
+options = DocTestDefaults(nthreads=0, long=False, optional="sage,gurobi", force_lib=True, abspath=True)
+
+import os, sage.env, glob, sys
+abs_file = os.path.abspath("check_sage_testsuite.py")
 os.chdir(os.path.join(sage.env.SAGE_LIB, 'sage'))
-os.system("sage-runtests -p "
-          "coding "
-          "combinat/designs combinat/integer_vector.py combinat/posets/ "
-          "game_theory/ "
-          "geometry/polyhedron/base.py geometry/cone.py "
-          "graphs/ "
-          "homology/simplicial_complex.py "
-          "knots/ "
-          "matroids/ "
-          "numerical/*.py* "
-          "sat/ ")
+files = ["coding",
+         "combinat/designs", "combinat/integer_vector.py", "combinat/posets/",
+         "game_theory/",
+         "geometry/polyhedron/base.py", "geometry/cone.py",
+         "graphs/",
+         "homology/simplicial_complex.py",
+         "knots/",
+         "matroids/",
+         "sat/"]
+
+files += glob.glob("numerical/*.py*")
+
+# First verify that we installed the default backend
+DC = DocTestController(options, [abs_file])
+err = DC.run()
+if err != 0:
+    print("Error: Setting the default solver did not work", file=sys.stderr)
+    sys.exit(2)
+
+# from $SAGE_SRC/bin/sage-runtests
+DC = DocTestController(options, files)
+err = DC.run()
+if err != 0:
+    sys.exit(1)
+
+sys.exit(0)
