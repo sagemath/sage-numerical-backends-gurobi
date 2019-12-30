0dac9b5c9972ad4a3a3a709c3a7db66fccde850c
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2019-12-30 18:36:17 -0500

    rename test_all.sh -> patch_and_check.sh, don't check the sage test suite here

--- patch_and_check.sh
+++ patch_and_check.sh
@@ -9,7 +9,6 @@ case $? in
         # Has license
         sage -c 'load("patch_into_sage_module.py")' || exit 1
         sage -c 'load("check_get_solver_with_name.py")' || exit 1
-        sage -c 'load("check_sage_testsuite.py")' || echo "Ignoring errors from the sage test suite"
         exit 0
         ;;
     *)
