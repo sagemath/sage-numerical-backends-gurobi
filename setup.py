508097caf8181ff04db9bdb65a5880a4205873b7
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-05-18 14:07:38 -0700

    setup.py: Add newer python versions to metadata

--- setup.py
+++ setup.py
@@ -160,6 +160,8 @@ setup(
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
                  ],
     ext_modules = cythonize(ext_modules, include_path=sys.path,
                             compile_time_env=compile_time_env),
